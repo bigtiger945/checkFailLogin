51c2dd4c56d493585a68b8c92844243a89b00e96
bigtiger945 <bigtiger945@gmail.com>
2021-02-10 13:07:52 +0800

    2021-02-10

--- checkFailLogin.sh
+++ checkFailLogin.sh
@@ -2,7 +2,7 @@
 currentDir = $(cd $(dirname $0)
 lastb >/dev/shm/lastb.txt
 rm /dev/shm/desc.txt
-/usr/bin/php /home/3w/sites/checkFailLogin/checkFailLogin.php
+/usr/bin/php /www/checkFailLogin/checkFailLogin.php
 if [ -f /dev/shm/desc.txt ]; then 
 	cat /dev/shm/desc.txt >> /etc/hosts.deny
 fi
